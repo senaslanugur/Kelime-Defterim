03db5ce6c3a8f20739e8d49e68239945b701d102
senaslanugur <91412306+senaslanugur@users.noreply.github.com>
2021-09-27 10:49:34 +0300

    Update dinleyerek_ogren.js

--- main_js/dinleyerek_ogren.js
+++ main_js/dinleyerek_ogren.js
@@ -1,23 +1,15 @@
     var hst = document.getElementById("makale_girisi");
-    var get_url = 'https://res.cloudinary.com/senaslanugur/raw/upload/v1/dinleme.json';
-    $.getJSON(get_url, function(d) {
-
 
         for(let i=0; i<70; i++){
-          
-
-                hst.innerHTML += "<div class='paper'><img class='poster' src='https://cdn-icons-png.flaticon.com/512/2991/2991595.png'  width='170' height='170'/><h2>Podcast</h2><h1>"+d[i].title+"</h1><a class='btn' href='#0' onclick = 'reply_click(this.id)' id='"+d[i].audio_list+"'>Hemen Dinle</a><div class='space'></div>";
+                hst.innerHTML += "<div class='paper'><img class='poster' src='https://cdn-icons-png.flaticon.com/512/2991/2991595.png'  width='170' height='170'/><h2>Podcast</h2><h1>"+data[i].title+"</h1><a class='btn' href='#0' onclick = 'reply_click(this.id)' id='"+data[i].audio_list+"'>Hemen Dinle</a><div class='space'></div>";
 
         }
-
-    });
-
                 function reply_click(clicked_id){
 
                     Swal.fire({
               title: 'Podcast',
               html:
-                 '<audio controls><source src="'+clicked_id+'" preload="auto" ></audio>',                     
+                 '<audio controls><source src="'+clicked_id+'" preload="auto" ></audio>',
               showCloseButton: false,
               showCancelButton: false,
               focusConfirm: false,
